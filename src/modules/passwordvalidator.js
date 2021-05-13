df1d1e79d8160b6db7cbe2e4508d74b52a03bd38
anna-devil-op <anna-devil-op@disnae.org>
2021-05-13 17:41:30 +0100

    Fixed the validation message for symbols shown to the user - was previously in the form of a regex. Also changed the minimum password length to 10 (it's a bit 'cleaner' than 11 but still arbitrary and should probably be higher but this would discount my usual 11 character password)

--- src/modules/passwordvalidator.js
+++ src/modules/passwordvalidator.js
@@ -6,8 +6,9 @@ class ValidationError extends Error {
 }
 
 const PasswordValidator = (function () {
-  const MINIMUM_PASSWORD_LENGTH = 11
-  const PASSWORD_SYMBOLS = /[!"#$%&'()*+,-./:;<=>?@[\\\]^_`{|}~]/
+  const MINIMUM_PASSWORD_LENGTH = 10
+  const PASSWORD_SYMBOLS = '!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~'
+  const PASSWORD_SYMBOLS_RX = new RegExp(`[${PASSWORD_SYMBOLS}]`)
 
   const _assert = (booleanValue, message) => {
     if (!booleanValue) {
@@ -22,7 +23,7 @@ const PasswordValidator = (function () {
     _assert(password.match(/[a-z]/), 'must contain at least one lowercase letter')
     _assert(password.match(/[A-Z]/), 'must contain at least one uppercase letter')
     _assert(password.match(/\d/), 'must contain at least one number')
-    _assert(password.match(PASSWORD_SYMBOLS), `must contain at least one symbol from: ${PASSWORD_SYMBOLS.toString()}`)
+    _assert(password.match(PASSWORD_SYMBOLS_RX), `must contain at least one symbol from: ${PASSWORD_SYMBOLS}`)
   }
 
   return {
